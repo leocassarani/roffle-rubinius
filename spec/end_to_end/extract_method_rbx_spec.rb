4b4e4d9a2326dd8a681be0b91dc0521238b59492
Leo Cassarani <leo.cassarani@me.com>
2012-07-02 00:56:06 +0100

    Indent heredocs

--- spec/end_to_end/extract_method_rbx_spec.rb
+++ spec/end_to_end/extract_method_rbx_spec.rb
@@ -8,39 +8,39 @@ require 'roffle/refactorings/extract_method'
 describe 'extracting a method' do
   it 'extracts without any local variables or args' do
     code = <<-RUBY
-def foo
-  puts "123"
-end
-RUBY
+      def foo
+        puts "123"
+      end
+    RUBY
     extracted_code = <<-RUBY
-def bar
-  puts "123"
-end
+      def bar
+        puts "123"
+      end
 
-def foo
-  bar
-end
-RUBY
+      def foo
+        bar
+      end
+    RUBY
     Roffle::Refactorings::ExtractMethod.extract_method(code, 'bar', 2, 2).should be_a_refactoring_of(extracted_code)
   end
 
   it 'ignores lines outside of the refactoring' do
     code = <<-RUBY
-def foo
-  puts "123"
-  puts "345"
-end
-RUBY
+      def foo
+        puts "123"
+        puts "345"
+      end
+    RUBY
     extracted_code = <<-RUBY
-def bar
-  puts "123"
-end
+      def bar
+        puts "123"
+      end
 
-def foo
-  bar
-  puts "345"
-end
-RUBY
+      def foo
+        bar
+        puts "345"
+      end
+    RUBY
     Roffle::Refactorings::ExtractMethod.extract_method(code, 'bar', 2, 2).should be_a_refactoring_of(extracted_code)
   end
 end
